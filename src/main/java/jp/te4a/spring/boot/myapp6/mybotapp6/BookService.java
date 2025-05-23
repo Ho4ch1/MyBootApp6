4a71f0d0b4b84af53b59e2f90e81f27a85ce5992
Shoki <syoki0415@gmail.com>
2025-05-23 14:03:53 +0900

    first commit

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/mybotapp6/BookService.java
@@ -0,0 +1,17 @@
+package jp.te4a.spring.boot.myapp6.mybotapp6;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+@Service
+public class BookService {
+ @Autowired
+ BookRepository bookRepository;
+ public BookBean save(BookBean bookBean) {
+    return bookRepository.save(bookBean);
+    }
+    public List<BookBean> findAll() {
+    return bookRepository.findAll();
+    }
+   }
---
+++ src/main/java/jp/te4a/spring/boot/myapp6/mybotapp6/Mybotapp6Application.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp6.mybotapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybotapp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mybotapp6Application.class, args);
+	}
+
+}
