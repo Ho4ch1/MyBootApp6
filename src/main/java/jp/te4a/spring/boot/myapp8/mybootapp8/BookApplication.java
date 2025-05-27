d0e065057de4c61105877f0480e99333b6787d03
Shoki <syoki0415@gmail.com>
2025-05-27 11:10:48 +0900

    commit kadai8

--- src/main/java/jp/te4a/spring/boot/myapp8/mybootapp8/BookApplication.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/mybootapp8/BookApplication.java
@@ -1,13 +1,13 @@
-package jp.te4a.spring.boot.myapp6.mybotapp6;
+package jp.te4a.spring.boot.myapp7.mybotapp7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Mybotapp6Application {
+public class Mybootapp7Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mybotapp6Application.class, args);
+		SpringApplication.run(Mybootapp7Application.class, args);
 	}
 
 }
