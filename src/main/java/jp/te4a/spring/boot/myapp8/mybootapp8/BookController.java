0673d43ed33a821987d111e30a13b5f7f8e894aa
Shoki <syoki0415@gmail.com>
2025-05-27 12:28:31 +0900

    commit kadai8

--- src/main/java/jp/te4a/spring/boot/myapp8/mybootapp8/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/mybootapp8/BookController.java
@@ -1,8 +1,8 @@
 package jp.te4a.spring.boot.myapp8.mybootapp8;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 
@@ -11,23 +11,38 @@ import org.springframework.ui.Model;
 public class BookController {
     @Autowired
     BookService bookService;
-    @RequestMapping("books/list")
-    public String index(Model model) {
-        model.addAttribute("msg", "this is setting message");
+    @ModelAttribute
+    BookForm setUpForm() {
+        return new BookForm();
+    }
+    @GetMapping
+    String list(Model model) {
+        model.addAttribute("books", bookService.findAll());
         return "books/list";
     }
-    @RequestMapping(value="books/list", method=RequestMethod.POST)
-    public ModelAndView postForm(@RequestParam("id") String id, 
-    @RequestParam("title") String title,@RequestParam("writter") String writter, 
-    @RequestParam("publisher") String publisher,@RequestParam("price") String price) {
-        ModelAndView mv = new ModelAndView("books/test");
-        bookService.save(new BookBean(Integer.valueOf(id), title, writter, publisher, Integer.valueOf(price)));
-        StringBuffer buff = new StringBuffer();
-        buff.append("<HR>");
-        for(BookBean bean:bookService.findAll()) {
-            buff.append("ID:" + bean.getId() + "<BR>" + "タイトル:" + bean.getTitle() + "<BR>"+ "著者:" + bean.getWritter() + "<BR>" + "出版社:" + bean.getPublisher() + "<BR>"+ "価格:" + bean.getPrice() + "<BR><HR>");
-        }
-        mv.addObject("books", bookService.findAll());
-        return mv;
+    @PostMapping(path="create")
+    String create(BookForm form, Model mode) {
+        bookService.create(form);
+        return "redirect:/books";
+    }
+    @PostMapping(path = "edit", params = "form")
+    String editForm(@RequestParam Integer id, BookForm form) {
+        BookForm bookForm = bookService.findOne(id);
+        BeanUtils.copyProperties(bookForm, form);
+        return "books/edit";
+    }
+    @PostMapping(path = "edit")
+    String edit(@RequestParam Integer id, BookForm form) {
+        bookService.update(form);
+        return "redirect:/books";
+    }
+    @PostMapping(path = "delete")
+    String delete(@RequestParam Integer id) {
+        bookService.delete(id);
+        return "redirect:/books";
+    }
+    @PostMapping(path = "edit", params = "goToTop")
+    String goToTop() {
+        return "redirect:/books";
     }
 }
