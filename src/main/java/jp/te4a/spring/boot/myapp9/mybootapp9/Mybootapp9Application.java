02cf063a2e0cd6c132608aef1ea15a2a581c3305
Shoki <syoki0415@gmail.com>
2025-05-28 12:53:25 +0900

    commit kadai9

--- src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/Mybootapp9Application.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/Mybootapp9Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Mybootapp8Application {
+public class Mybootapp9Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mybootapp8Application.class, args);
+		SpringApplication.run(Mybootapp9Application.class, args);
 	}
 
 }
