f21f71b4066012f4cf205178e9e6987528f21e4f
Shoki <syoki0415@gmail.com>
2025-06-02 09:56:12 +0900

    commit kadai8

--- src/test/java/jp/te4a/spring/boot/myapp6/mybotapp6/Mybotapp8ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp6/mybotapp6/Mybotapp8ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Mybotapp6ApplicationTests {
+class Mybotapp8ApplicationTests {
 
 	@Test
 	void contextLoads() {
